e28fa323821ee14c3aa053345a62ccb19c8ef8c3
dd3dd <jacky81810@gmail.com>
2023-09-21 17:42:19 +0800

    0921v5

--- src/MyH1.jsx
+++ src/MyH1.jsx
@@ -2,7 +2,7 @@ import './MyH1.css'
 export default function MyH1({ selectedValue }) {
     return (
         <div className='MyH1'>
-            {selectedValue !== '' ? <h1 >{selectedValue}</h1> : <h1 >天氣預報系統</h1>}
+            {selectedValue !== '' ? <h1 >{selectedValue}</h1> : <h1 >全臺天氣預報</h1>}
         </div>
     )
 }
